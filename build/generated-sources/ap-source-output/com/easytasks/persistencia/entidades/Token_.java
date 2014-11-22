fabfda7925f658a6239ca0ba9c65bec5ebc311dc
aletocar <aletocar@hotmail.com>
2014-11-22 15:27:36 -0200

    delete sin cascade con metodo feo para borrar los tokens

---
+++ build/generated-sources/ap-source-output/com/easytasks/persistencia/entidades/Token_.java
@@ -0,0 +1,16 @@
+package com.easytasks.persistencia.entidades;
+
+import com.easytasks.persistencia.entidades.Usuario;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.1.v20130918-rNA", date="2014-11-22T15:15:58")
+@StaticMetamodel(Token.class)
+public class Token_ { 
+
+    public static volatile SingularAttribute<Token, Usuario> usuario;
+    public static volatile SingularAttribute<Token, Long> id;
+    public static volatile SingularAttribute<Token, String> token;
+
+}
